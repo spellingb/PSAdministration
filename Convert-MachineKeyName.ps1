e8924c78438286a0cb5f8cf3b476bccdf9ac35f1
brandonspell08 <brandonspell08@users.noreply.github.com>
2018-11-10 20:27:06 -0600

    Update Convert-MachineKeyName.ps1

---
+++ Convert-MachineKeyName.ps1
@@ -0,0 +1,18 @@
+﻿Function Convert-MachineKeyName($fileobject){
+    if(!$fileobject){
+        $filename = ls "C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys"
+    } else {$filename = $fileobject }
+    foreach($file in $filename)
+    {
+        try{
+        $bytes = [system.io.file]::ReadAllBytes($file.fullname)
+        $ContainerName = [System.Text.Encoding]::ASCII.GetString($bytes, 40, $bytes[8]-1)
+        } Catch{
+        $ContainerName="No read access"
+        }
+        [PSCustomObject]@{
+            Container = $ContainerName
+            FileName = $file.Name
+            }
+    }
+}
