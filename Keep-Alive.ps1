cc34c326717461a4213b4c58c36d4a32ab13f9b7
spellingb <20215364+spellingb@users.noreply.github.com>
2021-08-03 09:14:39 -0500

    add keep-alive function

---
+++ Keep-Alive.ps1
@@ -0,0 +1,10 @@
+﻿
+$WShell = New-Object -com "Wscript.Shell"
+while ($true){
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Milliseconds 100
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Seconds 200
+  Prompt
+}
+
